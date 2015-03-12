68886cd818f24aae251350cd1a336d6813914a9d
Thomas Dashney <tdashney@gmail.com>
2015-03-12 13:00:26 -0400

    add boilerplate files from heroku example app

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/subreddit_exists', function(req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Reddit stats server listening at http://%s:%s', host, port);
