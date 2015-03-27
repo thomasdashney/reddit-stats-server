89b5b0883eed0f8b1d9b9d5f0702b09f82c9b65f
Thomas Dashney <tdashney@gmail.com>
2015-03-26 20:11:12 -0400

    changed to use sub# and return multiple searches in same response

--- server.js
+++ server.js
@@ -16,57 +16,90 @@ app.get('/', function(req, res) {
  * based on the query in the subreddit. It also represents subreddit statistics
  * 
  * @param  {string} q  The query string to search for
- * @param  {string} subreddit The subreddit
+ * @param  {string} sub1 Subreddit to search with
+ * @param  {string} sub2 Subreddit to search with
+ * @param  {string} sub3 Subreddit to search with
+ * @param  {string} sub4 Subreddit to search with
  *
- * Example response:
+ * Example response: (if subreddits were cities)
  * {
- *   "number_of_posts": "80",
- *   "score": "23014",
- *   "number_of_comments": "9727192"
+ *   "toronto": {
+ *     "score": 2512,
+ *     "number_of_posts": 35,
+ *     "number_of_comments": 4035
+ *   },
+ *   "montreal": {
+ *     "score": 2512,
+ *     "number_of_posts": 35,
+ *     "number_of_comments": 4035
+ *   },
+ *   "kingston": {
+ *     "score": 2512,
+ *     "number_of_posts": 35,
+ *     "number_of_comments": 4035
+ *   }
  * }
  * 
  * @return {[type]}      [description]
  */
 app.get('/get_data', function(req, res) {
-  if (!req.query.subreddit)
-    return res.status(400).send('No subreddit provided in query string');
+  if (!req.query.sub1 && !req.query.sub2 || !req.query.sub3 || !req.query.sub4)
+    return res.status(400).send('No subreddits provided');
   if (!req.query.q)
     return res.status(400).send('No q string provided');
   if (!req.query.q.length >= 512)
     return res.status(400).send('q must be smaller than 512 characters');
 
-  // results object to be populated
-  var result = {
-    number_of_posts: 0,
-    score: 0,
-    number_of_comments: 0
-  };
+  var subreddits = [];
+  // get our subreddits from the query string
+  if (req.query.sub1)
+    subreddits.push(req.query.sub1);
+  if (req.query.sub2)
+    subreddits.push(req.query.sub2);
+  if (req.query.sub3)
+    subreddits.push(req.query.sub3);
+  if (req.query.sub4)
+    subreddits.push(req.query.sub4);
 
-  var url = baseUrl+'/r/'+encodeURIComponent(req.query.subreddit)+'/search.json';
-  request({
-    url: url,
-    useQuerystring:true,
-    qs: {
-      access_token: config.redditApiKey,
-      q: req.query.q,
-      limit: 100,
-      restrict_sr: true,
-      t: 'all',
-      sort: 'top'
-    }
-  }, function(err, response, body) {
-    var data = JSON.parse(body).data;
-    if (!data.children)
-      return res.send({
-        message: 'no results'
+  async.map(subreddits, function getData(subreddit, cb) {
+
+    // results object to be populated
+    var result = {
+      number_of_posts: 0,
+      score: 0,
+      number_of_comments: 0
+    };
+
+    var url = baseUrl+'/r/'+encodeURIComponent(subreddit)+'/search.json';
+    request({
+      url: url,
+      useQuerystring:true,
+      qs: {
+        access_token: config.redditApiKey,
+        q: req.query.q,
+        limit: 100,
+        restrict_sr: true,
+        t: 'all',
+        sort: 'top'
+      }
+    }, function(err, response, body) {
+      var data = JSON.parse(body).data;
+      if (!data.children)
+        return res.send({
+          message: 'no results'
+        });
+      _.forEach(data.children, function(obj, key) {
+        result.score += obj.data.score;
+        result.number_of_comments += obj.data.num_comments;
       });
-    _.forEach(data.children, function(obj, key) {
-      result.score += obj.data.score;
-      result.number_of_comments += obj.data.num_comments;
+      // set number of posts to children
+      result.number_of_posts = data.children.length;
+      var wrapper = {};
+      wrapper[subreddit] = result;
+      cb(null, wrapper);
     });
-    // set number of posts to children
-    result.number_of_posts = data.children.length;
-    return res.json(result);
+  }, function returnResponse(err, results) {
+    return res.json(results);
   });
 });
 
